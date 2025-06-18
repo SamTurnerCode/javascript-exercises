7850e425ea992126521efe5221f9abc3750c8e94
Sam Turner <samturnerstudio@outlook.com>
2025-06-18 16:12:37 +1000

    Complete exercise 10

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+if(min < 0 || max < 0) return "ERROR"
+if(!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"
+
+if (min > max){
+    temp = max
+    max = min
+    min = temp
+}
+
+result = 0;
+for (let i = min; i <= max; i++){
+    result += i;
+}
+
+return result;
 
 };
 
