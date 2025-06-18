8aa42efa1fa30d085bac773eb916ca00ba5091b0
Sam Turner <samturnerstudio@outlook.com>
2025-06-18 15:36:39 +1000

    Complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeValues) {
+
+    resultArray = [];
+    for (let i = 0; i < array.length; i++){
+        if(!removeValues.includes(array[i])){
+            resultArray.push(array[i])
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
