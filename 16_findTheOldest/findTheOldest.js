03a85c475bad6be0541b38841f91d8b69a50590f
Sam Turner <samturnerstudio@outlook.com>
2025-07-08 12:43:29 +1000

    Complete javascript exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,41 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest;
+    const findAge = function(person){
+        if(!person.yearOfDeath){
+            let currentYear = new Date().getFullYear();
+            return  currentYear - person.yearOfBirth;
+        }
+        return person.yearOfDeath - person.yearOfBirth
+    }
+    for(let i in people){
+        if(!oldest){
+            oldest = people[i];
+        }
+        if(findAge(oldest) < findAge(people[i])){
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
+
+findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
